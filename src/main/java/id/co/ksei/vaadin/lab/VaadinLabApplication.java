a8e4821a85f207a2d35a35b6e39e32dd91fd1a27
Singgih Yunanto <singgih.yunanto@fusi24.com>
2025-12-15 14:56:30 +0700

    initial commit

---
+++ src/main/java/id/co/ksei/vaadin/lab/VaadinLabApplication.java
@@ -0,0 +1,13 @@
+package id.co.ksei.vaadin.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/ksei/vaadin/lab/VaadinLabApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.ksei.vaadin.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
